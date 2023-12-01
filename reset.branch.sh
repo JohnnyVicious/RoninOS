f80ea3380b5b7995f3b583b043c69d9f90e7feb0
Damm Kewl <25-dammkewl@users.noreply.code.samourai.io>
2023-12-01 22:56:48 +0100

    fixed a missing sudo in the image altering script

--- reset.branch.sh
+++ reset.branch.sh
@@ -12,6 +12,6 @@ startbyte=$(($sectorsize*$startsector))
 mkdir ronin_temp_mount
 sudo mount -o loop,offset=$startbyte Armbian* ./ronin_temp_mount
 sudo sed -i 's|^.*git clone.*$|git clone -b master https://code.samourai.io/ronindojo/RoninDojo /home/ronindojo/RoninDojo|' ronin_temp_mount/usr/local/sbin/ronin-setup.sh
-umount ronin_temp_mount
+sudo umount ronin_temp_mount
 rmdir ronin_temp_mount
 echo "done"
