8d30b5e900108c749b26ba16bd7c61a93220c1d3
Johnny <6608571+JohnnyVicious@users.noreply.github.com>
2023-12-28 18:47:06 +0100

    Update ronin-teardown.sh

--- overlays/RoninOS/usr/local/sbin/ronin-teardown.sh
+++ overlays/RoninOS/usr/local/sbin/ronin-teardown.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+[ ! -f /home/ronindojo/.logs/pre-complete ] && sleep 30s; exit 0;
+
 echo "Waiting to resurrect PM2"
 while [ ! -f /home/ronindojo/.logs/setup-complete ]
 do
    echo "waiting..."
-   sleep 5s
+   sleep 30s
    if [ -f /home/ronindojo/.logs/setup-complete ]; then
       if ! systemctl is-active pm2-ronindojo.service; then
         sudo systemctl start pm2-ronindojo.service
