d88d2e00c9770f1995877fb0a2f0b71a5e80fde7
Johnny <6608571+JohnnyVicious@users.noreply.github.com>
2023-12-28 10:12:18 +0100

    Update ronin-setup.sh

--- overlays/RoninOS/usr/local/sbin/ronin-setup.sh
+++ overlays/RoninOS/usr/local/sbin/ronin-setup.sh
@@ -1,53 +1,23 @@
 #!/bin/bash
 
-regenerate_passwords_and_update_info_file(){
-    # Generate a random password consisting of 16 characters using only numbers and letters.
-    rootpwd="$(tr -dc 'a-zA-Z0-9' </dev/urandom | head -c'21')"
-    roninpwd="$(tr -dc 'a-zA-Z0-9' </dev/urandom | head -c'21')"
-    USER="ronindojo"
+# This service will run as the $USER, passwordless sudo should have been set at this point
 
-
-    # Change the root user's password.
-    echo "Changing the root user's password..."
-    sudo chpasswd <<<"root:$rootpwd"
-
-    # Change the ronindojo user's password.
-    echo "Changing the ronindojo user's password..."
-    sudo chpasswd <<<"$USER:$roninpwd"
-
-    # remove old info.json then create new with new passwords.
-    rm -rf /home/ronindojo/.config/RoninDojo/info.json
-    cat <<EOF >/home/"${USER}"/.config/RoninDojo/info.json
-{"user":[{"name":"${USER}","password":"${roninpwd}"},{"name":"root","password":"${rootpwd}"}]}
-EOF
-
-    # add validation for that the setup was done.
-    GENERATE_MESSAGE="Your password was randomly generated during System Setup."
-    TIMESTAMP=$(date)
-    cat <<EOF >/home/"${USER}"/.logs/pass_gen_timestamp.txt
-$GENERATE_MESSAGE
-Date and Time: $TIMESTAMP
-EOF
-}
-
+# Continue only if you can access the service user's home folder
 cd "$HOME" || exit
 
 # give time for Startup to finish before trying to update the repo. 
-sleep 75s 
+sleep 30s 
 
 # Clone Repo
 git clone -b master https://code.samourai.io/ronindojo/RoninDojo /home/ronindojo/RoninDojo
 cd /home/ronindojo/RoninDojo
 
-# Source files
+# Source files for default values and generic functions
 . Scripts/defaults.sh
 . Scripts/functions.sh
 
 # Run main
 if _main; then
-    # regenerate the passwords to be random for each user.
-    regenerate_passwords_and_update_info_file
-
     # Run system setup
     Scripts/Install/install-system-setup.sh system
 
@@ -60,6 +30,5 @@ if _main; then
         sudo systemctl start pm2-ronindojo.service
     fi
     sudo systemctl disable ronin-setup.service
-    sudo sed -i '/ronindojo/s/ALL) NOPASSWD:ALL/ALL) ALL/' /etc/sudoers
     touch /home/ronindojo/.logs/setup-complete
 fi
