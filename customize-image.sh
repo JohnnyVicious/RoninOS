c39a632aab3d9ec05aa51f9e7233187587fd538c
Johnny <6608571+JohnnyVicious@users.noreply.github.com>
2023-12-29 10:39:16 +0100

    Update customize-image.sh

--- customize-image.sh
+++ customize-image.sh
@@ -138,28 +138,27 @@ _service_checks(){
     fi
 }
 
-# Installs Nodejs, docker, docker-compose, and pm2. Clones the RoninDojo repo. This is needed due to some out dated packages in the default debian package manager.
 _prep_install(){
-    # install Nodejs
+    echo "Installing Nodejs"
     curl -sL https://deb.nodesource.com/setup_16.x | bash -
     apt-get update
     apt-get install -y nodejs
 
-    # install docker
+    echo "Installing Docker on $DISTRO release $RELEASE"
     mkdir -m 0755 -p /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/"$(lsb_release -is)"/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$(lsb_release -is) \
     $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-
     apt-get update
     apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-    # install docker-compose
-    curl -L https://github.com/docker/compose/releases/download/v2.0.1/docker-compose-linux-aarch64 -o /usr/bin/docker-compose
+    echo "Installing docker-compose"
+    ARCHICTECTURE="aarch64"
+    curl -L https://github.com/docker/compose/releases/download/v2.0.1/docker-compose-linux-"$ARCHITECTURE" -o /usr/bin/docker-compose
     chmod +x /usr/bin/docker-compose
 
-    # install pm2 
+    echo "Installing pm2"
     npm install pm2 -g
 }
 
@@ -284,7 +283,7 @@ main(){
     )    
 
     apt install -y lsb-release
-    distro=$(lsb_release -is)
+    export distro=$(lsb_release -is)
     release=$(lsb_release -cs)
 
     case $distro in
