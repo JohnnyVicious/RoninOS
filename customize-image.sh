999e4584c9343bbfb704dc5cfb74f81b3c7275a1
Johnny <6608571+JohnnyVicious@users.noreply.github.com>
2023-12-29 10:22:17 +0100

    Update customize-image.sh

--- customize-image.sh
+++ customize-image.sh
@@ -147,9 +147,9 @@ _prep_install(){
 
     # install docker
     mkdir -m 0755 -p /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    curl -fsSL https://download.docker.com/linux/"$(lsb_release -is)"/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$(lsb_release -is) \
     $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
     apt-get update
