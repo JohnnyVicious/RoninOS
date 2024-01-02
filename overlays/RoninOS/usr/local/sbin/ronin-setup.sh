5472543d0a0782d990aa1bb329166d64dc233c8d
Johnny <6608571+JohnnyVicious@users.noreply.github.com>
2024-01-02 13:53:52 +0100

    Update ronin-setup.sh

--- overlays/RoninOS/usr/local/sbin/ronin-setup.sh
+++ overlays/RoninOS/usr/local/sbin/ronin-setup.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
 # This service will run as the $USER, passwordless sudo should have been set at this point
+echo "Check if passwordless sudo is enabled for user $USER"  
+if sudo -n true 2>/dev/null; then
+    echo "User $USER has passwordless sudo access."
+else
+    echo "Error: User $USER does not have passwordless sudo access";
+    cat "$HOME"/.config/RoninDojo/config.json
+    exit 1;
+fi
 
-[ -f /home/ronindojo/.logs/setup-complete ] && (echo "Setup has already run, exiting..."; sudo systemctl disable ronin-setup.service; exit 0;)
+[ -f "$HOME"/.logs/setup-complete ] && (echo "Setup has already run, you should disable ronin-setup.service by using root."; exit 0;)
 
-while [ ! -f /home/ronindojo/.logs/presetup-complete ]
+[ -f "$HOME"/.config/RoninDojo/config.json ] && (echo "File $HOME/.config/RoninDojo/config.json is missing!"; exit 1;)
+
+while [ ! -f "$HOME"/.logs/presetup-complete ]
 do
    echo "waiting until pre-setup is complete..."
    sleep 30s
-   if [ -f /home/ronindojo/.logs/presetup-complete ]; then
+   if [ -f "$HOME"/.logs/presetup-complete ]; then
       break
    fi
 done
@@ -17,28 +27,18 @@ echo "Continue only if you can access the service user's home folder"
 cd "$HOME" || exit 1;
 
 echo "Give time for Startup to finish before trying to clone the repo"
-sleep 30s
+sleep 75s
 REPO="--branch master https://code.samourai.io/ronindojo/RoninDojo.git"
-[ ! -d /home/ronindojo/RoninDojo ] && (echo "Cloning repo : $(echo $REPO)"; git clone $(echo "$REPO") /home/ronindojo/RoninDojo)
-[ ! -d /home/ronindojo/RoninDojo ] && (echo "Cloning repo failed!"; exit 1;)
-cd /home/ronindojo/RoninDojo || exit 1;
+[ ! -d "$HOME"/RoninDojo ] && (echo "Cloning repo : $(echo $REPO)"; git clone $(echo "$REPO") "$HOME"/RoninDojo)
+[ ! -d "$HOME"/RoninDojo ] && (echo "Cloning repo failed!"; exit 1;)
+cd "$HOME"/RoninDojo || exit 1;
 
 # Source files for default values and generic functions
 . Scripts/defaults.sh
 . Scripts/functions.sh
 
 # Run main
-if _main; then
-    echo "Check if passwordless sudo is enabled for user $USER"
-    
-    if sudo -n true 2>/dev/null; then
-        echo "User $USER has passwordless sudo access."
-    else
-        echo "Error: User $USER does not have passwordless sudo access";
-        cat /home/"$USER"/.config/RoninDojo/config.json
-        exit 1;
-    fi
-    
+if _main; then    
     # Run system setup
     Scripts/Install/install-system-setup.sh system
 
@@ -53,6 +53,8 @@ if _main; then
     if ! systemctl is-active pm2-ronindojo.service; then
         sudo systemctl start pm2-ronindojo.service
     fi    
-    sudo systemctl disable ronin-setup.service    
-    touch /home/ronindojo/.logs/setup-complete
+    sudo systemctl disable ronin-setup.service            
+    # Disable passwordless sudo
+    sudo sed -i '/ronindojo/s/ALL) NOPASSWD:ALL/ALL) ALL/' /etc/sudoers
+    touch "$HOME"/.logs/setup-complete    
 fi
