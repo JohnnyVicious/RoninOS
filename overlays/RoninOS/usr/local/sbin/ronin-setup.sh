93cbd84fd308ec1c8e4ca750d9fc6b2e1396358b
Johnny <6608571+JohnnyVicious@users.noreply.github.com>
2024-01-03 23:09:59 +0100

    Update ronin-setup.sh

--- overlays/RoninOS/usr/local/sbin/ronin-setup.sh
+++ overlays/RoninOS/usr/local/sbin/ronin-setup.sh
@@ -50,7 +50,9 @@ if _main; then
     
     sudo apt-get purge -y --autoremove nodejs npm
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-    # source ~/.bashrc    
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"   
     nvm install 16
     nvm use 16
     nvm alias default 16
