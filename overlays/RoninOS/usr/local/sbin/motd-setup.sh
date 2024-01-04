1e17fa9642bb3d3d84f975958b6dd0f1b6996dc7
Johnny <6608571+JohnnyVicious@users.noreply.github.com>
2024-01-04 22:04:59 +0100

    Update motd-setup.sh

--- overlays/RoninOS/usr/local/sbin/motd-setup.sh
+++ overlays/RoninOS/usr/local/sbin/motd-setup.sh
@@ -7,12 +7,6 @@ Welcome to RoninDojo!
 
 Website:   ronindojo.io
 Wiki:      wiki.ronindojo.io
-
-Nodejs:    echo "$(node -v)"
-NPM:       echo "$(npm -v)"
-PM2:       echo "$(pm2 -v)"
-PNPM:      echo "$(pnpm -v)"
-Docker:    echo "$(docker --version | grep -oP 'Docker version \K[^,]+')"
 EOF"
 
 touch /tmp/motd-actived
