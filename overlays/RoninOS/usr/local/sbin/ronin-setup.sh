e94159689784fa0a368b8845f4fce892b195b2e7
Johnny <6608571+JohnnyVicious@users.noreply.github.com>
2024-01-05 10:27:07 +0100

    Update ronin-setup.sh

--- overlays/RoninOS/usr/local/sbin/ronin-setup.sh
+++ overlays/RoninOS/usr/local/sbin/ronin-setup.sh
@@ -118,6 +118,37 @@ if _main; then
         pm2 kill
         sudo systemctl start pm2-ronindojo
     fi
+    
+    # Add the following to the .profile to make RoninDojo CLI launch on SSH login
+    cat << 'EOF' > ~/.profile
+# ~/.profile: executed by the command interpreter for login shells.
+# This file is not read by bash(1), if ~/.bash_profile or ~/.bash_login
+# exists.
+# see /usr/share/doc/bash/examples/startup-files for examples.
+# the files are located in the bash-doc package.
+
+# the default umask is set in /etc/profile; for setting the umask
+# for ssh logins, install and configure the libpam-umask package.
+#umask 022
+
+# if running bash
+if [ -n "$BASH_VERSION" ]; then
+    # include .bashrc if it exists
+    if [ -f "$HOME/.bashrc" ]; then
+        . "$HOME/.bashrc"
+    fi
+fi
+
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/bin" ] ; then
+    PATH="$HOME/bin:$PATH"
+fi
+
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/.local/bin" ] ; then
+    PATH="$HOME/.local/bin:$PATH"
+fi
+EOF
 
     # TODO: could add some more checks to see if install completed succesfully, work in progress
 
