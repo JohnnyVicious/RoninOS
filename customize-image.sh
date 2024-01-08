59d46729dd4c6b1ae40284490fb3a00c54cd9f1a
Johnny <6608571+JohnnyVicious@users.noreply.github.com>
2024-01-08 14:22:49 +0100

    Update customize-image.sh

--- customize-image.sh
+++ customize-image.sh
@@ -165,12 +165,12 @@ _prep_install(){
     fi    
     if [ "$NODE_VERSION" -gt 16 ]; then
     	apt-get remove -y nodejs
-        apt install curl gpg gnupg2 software-properties-common apt-transport-https lsb-release ca-certificates
+        apt install -y curl gpg gnupg2 software-properties-common apt-transport-https lsb-release ca-certificates
         mkdir -p /etc/apt/keyrings
         curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 	NODE_MAJOR=16
         echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
-	sudo apt update && sudo apt install nodejs
+	sudo apt update && sudo apt install -y nodejs
         apt-mark hold nodejs
     fi
     
