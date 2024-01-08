43b59286453c13bd4e6e5432c2ce777edcb43285
Johnny <6608571+JohnnyVicious@users.noreply.github.com>
2024-01-08 14:26:04 +0100

    Update customize-image.sh

--- customize-image.sh
+++ customize-image.sh
@@ -170,7 +170,7 @@ _prep_install(){
         curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 	NODE_MAJOR=16
         echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
-	sudo apt update && sudo apt install -y nodejs
+	apt update && apt install -y nodejs
         apt-mark hold nodejs
     fi
     
